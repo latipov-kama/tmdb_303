a66eef8af8c8f9716bf122f8a151d6cfa4c9dd86
KamaProgman <latipovkamran1@gmail.com>
2025-03-26 17:31:02 +0500

    test

--- src/libs/utils.js
+++
@@ -1,9 +0,0 @@
-export function render(arr, playce, Component) {
-    playce.innerHTML = "";
-
-    arr.forEach(item => {
-        const element = Component(item);
-
-        playce.appendChild(element);
-    });
-}
